4b26e9c9596d86a2b3c6cf2c5b6d8d2db1f94366
iRandH <haykansyahputra27@gmail.com>
2025-12-27 12:02:54 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// route
+app.get("/", (req, res) => {
+  res.render("main"); // TANPA .ejs
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
